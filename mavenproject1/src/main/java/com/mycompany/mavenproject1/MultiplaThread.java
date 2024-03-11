bba544f56417d8d64a48cde741490de043c5a388
Jorge <Jorge@LAPTOP-13O2AL1N>
2024-03-11 15:06:53 -0300

    projeto inicial MultiThread

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/MultiplaThread.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Jorge
+ */
+import javax.swing.*;
+import java.awt.event.*; 
+
+public class MultiplaThread extends JFrame {
+   private JButton botao;
+   public MultiplaThread() {	
+      super("Aplicação MultiThread");   
+      System.out.println("Dentro da Janela:" + Thread.activeCount());		
+      botao = new JButton("botão");	 	 
+      this.getContentPane().add(botao);
+      this.pack();
+      botao.addActionListener( new ActionListener() {
+	public void actionPerformed(ActionEvent e) { 
+	    JOptionPane.showMessageDialog(null,"Pressionou Botão 1");
+	}
+      });
+   }
+}
+
